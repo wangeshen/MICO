ed77e8ebaa47fb6793137bdbf37cedd67ffdfb39
neooxu <xuwei107@me.com>
2014-09-18 10:54:35 +0800

    remove wlan_bus.h

--- Platform/Common/Cortex-M3/STM32F2xx/EMW1062_Driver/SDIO/wlan_bus.c
+++ Platform/Common/Cortex-M3/STM32F2xx/EMW1062_Driver/SDIO/wlan_bus.c
@@ -31,7 +31,6 @@
 
 #include "MicoRtos.h"
 #include "misc.h"
-#include "wlan_bus.h"
 #include "string.h" /* For memcpy */
 #include "gpio_irq.h"
 #include "platform_common_config.h"
@@ -65,6 +64,17 @@ extern void wake_up_interrupt_notify( void );
 
 #define SDIO_ERROR_MASK   ( SDIO_STA_CCRCFAIL | SDIO_STA_DCRCFAIL | SDIO_STA_CTIMEOUT | SDIO_STA_DTIMEOUT | SDIO_STA_TXUNDERR | SDIO_STA_RXOVERR | SDIO_STA_STBITERR )
 
+/**
+ * Transfer direction for the mico platform bus interface
+ */
+typedef enum
+{
+    /* If updating this enum, the bus_direction_mapping variable will also need to be updated */
+    BUS_READ,
+    BUS_WRITE
+} bus_transfer_direction_t;
+
+
 static const uint32_t bus_direction_mapping[] =
 {
     [BUS_READ]  = SDIO_TransferDir_ToSDIO,
@@ -82,6 +92,53 @@ static const uint32_t bus_direction_mapping[] =
 #define BUS_LEVEL_MAX_RETRIES   5
 
 #define SDIO_ENUMERATION_TIMEOUT_MS    (500)
+
+
+/******************************************************
+ *                   Enumerations
+ ******************************************************/
+
+/*
+ * SDIO specific constants
+ */
+typedef enum
+{
+    SDIO_CMD_0  =  0,
+    SDIO_CMD_3  =  3,
+    SDIO_CMD_5  =  5,
+    SDIO_CMD_7  =  7,
+    SDIO_CMD_52 = 52,
+    SDIO_CMD_53 = 53,
+    __MAX_VAL   = 64
+} sdio_command_t;
+
+typedef enum
+{
+    SDIO_BLOCK_MODE = ( 0 << 2 ), /* These are STM32 implementation specific */
+    SDIO_BYTE_MODE  = ( 1 << 2 )  /* These are STM32 implementation specific */
+} sdio_transfer_mode_t;
+
+typedef enum
+{
+    SDIO_1B_BLOCK    =  1,
+    SDIO_2B_BLOCK    =  2,
+    SDIO_4B_BLOCK    =  4,
+    SDIO_8B_BLOCK    =  8,
+    SDIO_16B_BLOCK   =  16,
+    SDIO_32B_BLOCK   =  32,
+    SDIO_64B_BLOCK   =  64,
+    SDIO_128B_BLOCK  =  128,
+    SDIO_256B_BLOCK  =  256,
+    SDIO_512B_BLOCK  =  512,
+    SDIO_1024B_BLOCK = 1024,
+    SDIO_2048B_BLOCK = 2048
+} sdio_block_size_t;
+
+typedef enum
+{
+    RESPONSE_NEEDED,
+    NO_RESPONSE
+} sdio_response_needed_t;
 
 /******************************************************
  *             Structures
