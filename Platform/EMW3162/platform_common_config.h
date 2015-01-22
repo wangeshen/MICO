47f7cb92e822d3899541dc14af928e27e82c4dd2
wangeshen <wangeshen@gmail.com>
2015-01-22 12:45:49 +0800

    update libEasyCloud to v0.2.7 && disable STDIO
    1. EasyCloud lib to version v0.2.7;
    2. disable STDIO in bootloader for LED use USART pin to control.

--- Platform/EMW3162/platform_common_config.h
+++ Platform/EMW3162/platform_common_config.h
@@ -48,7 +48,7 @@
 
 /************************************************************************
  * Uncomment to disable standard IO, i.e. printf(), etc. */
-//#define MICO_DISABLE_STDIO
+#define MICO_DISABLE_STDIO
 
 /************************************************************************
  * Uncomment to disable MCU powersave API functions */
